de56a8aec733b7c382a219f1380d6104e35701a2
Moshe Berman <moshberm@gmail.com>
2013-07-11 10:24:02 -0400

    Initial Commit

---
+++ SpiffyKit/SpiffyKit.h
@@ -0,0 +1,13 @@
+//
+//  SpiffyKit.h
+//  SpiffyKit
+//
+//  Created by Moshe Berman on 7/11/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SpiffyKit : NSObject
+
+@end
---
+++ SpiffyKit/SpiffyKit.m
@@ -0,0 +1,13 @@
+//
+//  SpiffyKit.m
+//  SpiffyKit
+//
+//  Created by Moshe Berman on 7/11/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "SpiffyKit.h"
+
+@implementation SpiffyKit
+
+@end
