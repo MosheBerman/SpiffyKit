6baa218cb259c2e9cf9c4f50b54b0ea9af5f5db8
Moshe Berman <moshberm@gmail.com>
2013-07-14 01:13:05 -0400

    Added a test app to the repo.

---
+++ SpiffyTester/SpiffyTester/STViewController.h
@@ -0,0 +1,13 @@
+//
+//  STViewController.h
+//  SpiffyTester
+//
+//  Created by Moshe Berman on 7/14/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STViewController : UIViewController
+
+@end
---
+++ SpiffyTester/SpiffyTester/STViewController.m
@@ -0,0 +1,29 @@
+//
+//  STViewController.m
+//  SpiffyTester
+//
+//  Created by Moshe Berman on 7/14/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "STViewController.h"
+
+@interface STViewController ()
+
+@end
+
+@implementation STViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
