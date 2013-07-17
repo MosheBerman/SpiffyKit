1ea7c4814327f1361b19b84e3a0c3970e7aeedc1
Moshe Berman <moshberm@gmail.com>
2013-07-17 00:36:44 -0400

    Added appColor to SpiffyController.

--- SpiffyKit/Public Controller/SpiffyController.h
+++ SpiffyKit/Public Controller/SpiffyController.h
@@ -19,6 +19,8 @@
 @property (nonatomic, strong) NSString *supportEmailAddress;
 @property (nonatomic, strong) NSString *twitterHandle;
 
+@property (nonatomic, strong) UIColor *appColor;
+
 + (SpiffyController *)sharedController;
 
 //	On iPad, pass a rect for popover. Else, pass CGRectZero.
