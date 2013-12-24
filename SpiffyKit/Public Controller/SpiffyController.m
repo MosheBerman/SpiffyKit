6aae1d255f8725ac3ea63c18b311196a137a8a5e
Moshe <moshberm@gmail.com>
2013-12-24 13:16:12 -0500

    Added flurry logging to several classes to track playback and other events.

--- SpiffyKit/Public Controller/SpiffyController.m
+++ SpiffyKit/Public Controller/SpiffyController.m
@@ -12,6 +12,9 @@
 
 #import <UIKit/UIKit.h>
 
+#import "Flurry.h"
+
+
 @interface SpiffyController () <UIPopoverControllerDelegate>
 
 @property (nonatomic, strong) UIPopoverController *popover;
@@ -23,14 +26,14 @@
 
 + (SpiffyController *)sharedController
 {
-		static SpiffyController *controller = nil;
-		
-		static dispatch_once_t onceToken;
-		dispatch_once(&onceToken, ^{
-				controller = [[SpiffyController alloc] init];
-		});
-		
-		return controller;
+    static SpiffyController *controller = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        controller = [[SpiffyController alloc] init];
+    });
+    
+    return controller;
 }
 
 - (id)init
@@ -38,7 +41,7 @@
     self = [super init];
     if (self) {
         _shouldPresentAnalytics = NO;
-				[self setDefaultsIfNecessary];
+        [self setDefaultsIfNecessary];
     }
     return self;
 }
@@ -46,25 +49,25 @@
 
 - (void)presentInViewController:(UIViewController *)viewController fromRectWhereApplicable:(CGRect)rect
 {
-		
-		if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-		{
-				[[self popover] presentPopoverFromRect:rect inView:viewController.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
-		}
-		else
-		{
-				[viewController presentViewController:[self spiffyViewController] animated:YES completion:nil];
-		}
+    
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+    {
+        [[self popover] presentPopoverFromRect:rect inView:viewController.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
+    else
+    {
+        [viewController presentViewController:[self spiffyViewController] animated:YES completion:nil];
+    }
 }
 
-#pragma mark - Spiffy 
+#pragma mark - Spiffy
 
 - (SpiffyViewController *)spiffyViewController
 {
-		if (!_spiffyViewController) {
-				_spiffyViewController = [[SpiffyViewController alloc] init];
-		}
-		return _spiffyViewController;
+    if (!_spiffyViewController) {
+        _spiffyViewController = [[SpiffyViewController alloc] init];
+    }
+    return _spiffyViewController;
 }
 
 #pragma mark - Popover
@@ -72,58 +75,72 @@
 
 - (UIPopoverController *)popover
 {
-		if (!_popover) {
-				_popover = [[UIPopoverController alloc] initWithContentViewController:[self spiffyViewController]];
-				[_popover setDelegate:self];
-		}
-		return _popover;
+    if (!_popover) {
+        _popover = [[UIPopoverController alloc] initWithContentViewController:[self spiffyViewController]];
+        [_popover setDelegate:self];
+    }
+    return _popover;
 }
 
 #pragma mark - Diagnostics
 
 - (void)toggleDiagnostics:(UISwitch *)sender
 {
-		BOOL enabled = [sender isOn];
-		
-		[[NSUserDefaults standardUserDefaults] setBool:enabled forKey:@"SpiffyKitDiagnosticsEnabled"];
+    BOOL enabled = [sender isOn];
+    
+    [[NSUserDefaults standardUserDefaults] setBool:enabled forKey:@"SpiffyKitDiagnosticsEnabled"];
+    
+    /** Log event if analytics are enabled. */
+    if([[SpiffyController sharedController] analyticsEnabled]) {
+        
+        NSDictionary *parameters = @{@"Enabled" : @(enabled)};
+        [Flurry logEvent:@"Toggling Diagnostics" withParameters:parameters];
+    }
+    
 }
 
 - (BOOL)diagnosticsEnabled
 {
-		return [[NSUserDefaults standardUserDefaults] boolForKey:@"SpiffyKitDiagnosticsEnabled"];
+    return [[NSUserDefaults standardUserDefaults] boolForKey:@"SpiffyKitDiagnosticsEnabled"];
 }
 
 #pragma mark - Analytics
 
 - (void)toggleAnalytics:(UISwitch *)sender
 {
-		BOOL enabled = [sender isOn];
-		
-		[[NSUserDefaults standardUserDefaults] setBool:enabled forKey:@"SpiffyKitAnalyticsEnabled"];
+    BOOL enabled = [sender isOn];
+    
+    [[NSUserDefaults standardUserDefaults] setBool:enabled forKey:@"SpiffyKitAnalyticsEnabled"];
+    
+    /** Log event if analytics are enabled. */
+    if([[SpiffyController sharedController] analyticsEnabled]) {
+        
+        NSDictionary *parameters = @{@"Enabled" : @(enabled)};
+        [Flurry logEvent:@"Toggling Analytics" withParameters:parameters];
+    }
 }
 
 - (BOOL)analyticsEnabled
 {
-		return [[NSUserDefaults standardUserDefaults] boolForKey:@"SpiffyKitAnalyticsEnabled"];
+    return [[NSUserDefaults standardUserDefaults] boolForKey:@"SpiffyKitAnalyticsEnabled"];
 }
 
 #pragma mark - Defaults
 
 - (void)setDefaultsIfNecessary
 {
-		if (![self userDefaultsContainsKey:@"SpiffyKitDiagnosticsEnabled"]) {
-				[[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"SpiffyKitDiagnosticsEnabled"];
-		}
-		
-		if (![self userDefaultsContainsKey:@"SpiffyKitAnalyticsEnabled"] && _shouldPresentAnalytics) {
-//				NSLog(@"Analytics aren't implemented, saving flag for future use.");
-				[[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"SpiffyKitAnalyticsEnabled"];
-		}
+    if (![self userDefaultsContainsKey:@"SpiffyKitDiagnosticsEnabled"]) {
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"SpiffyKitDiagnosticsEnabled"];
+    }
+    
+    if (![self userDefaultsContainsKey:@"SpiffyKitAnalyticsEnabled"]) {
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"SpiffyKitAnalyticsEnabled"];
+    }
 }
 
 - (BOOL)userDefaultsContainsKey:(NSString *)key
 {
-		return [[[[NSUserDefaults standardUserDefaults] dictionaryRepresentation] allKeys] containsObject:key];
+    return [[[[NSUserDefaults standardUserDefaults] dictionaryRepresentation] allKeys] containsObject:key];
 }
 
 @end
